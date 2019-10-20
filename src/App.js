82ba9fb1b1d1af16d9a1132e863a6fc66dbf0544
chingsley <eneja.kc@gmail.com>
2019-10-20 21:45:07 +0100

    -create Home and AvengersList pages -Add css for the the to design the AvengersList page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Home from './components/Home';
+import AvengersList from './components/AvengersList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>{`Let's begin`}</h1>
-    </div>
-  );
+import avengers from './data';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      avengers: avengers,
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+       <Route exact path="/" component={Home}/>
+       <Route path="/avengers" component={AvengersList} />
+      </div>
+    );
+  }
 }
 
 export default App;
