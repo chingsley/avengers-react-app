e1379b7b4ade761efcc6b19430e905867a2cbe81
chingsley <eneja.kc@gmail.com>
2019-10-25 10:11:27 +0100

    - implement router Links for navigation - css style, implement white theme

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return(
     <div>
       <h1>Avengers Database</h1>
       <h4>Danger! This is classified infromation!</h4>
+      <Link to="/avengers">Enter</Link>
     </div>
   );
 }
